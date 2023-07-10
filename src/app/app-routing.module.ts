d2d643b31640463192872e2031782de1dea508cc
Laureano Oliva <laureanoliva@gmail.com>
2023-07-10 17:37:41 -0300

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { CursoComponent } from './curso/curso.component';
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+  { path: 'curso', component: CursoComponent },
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  titulos = ['Matematica Discreta', 'Quimica', 'Analisis Matematico 2', 'Equipos y Procesos', 'Introduccion a la Universidad']
+}
