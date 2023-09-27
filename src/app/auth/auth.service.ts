c555c57cf89ab3084137ce349d2793a28b44cd62
Laureano Oliva <65315757+laucha54321@users.noreply.github.com>
2023-09-27 16:52:24 -0300

    fixed to connect to new api

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,70 +1,70 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Router } from "@angular/router"
-import { Subject } from "rxjs";
-import { tap } from "rxjs/operators";
-import { User } from "./user.model";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { User } from './user.model';
 
-export interface AuthResponseData{
-    id:string;
-    accessToken:string;
+export interface AuthResponseData {
+  email: string;
+  accessToken: string;
 }
 
-@Injectable({providedIn:'root'})
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  user = new Subject<User>();
 
-export class AuthService{
+  pepe() {
+    console.log('pepe');
+  }
 
-    user = new Subject<User>();
+  constructor(private http: HttpClient, private router: Router) {}
 
-    constructor(private http: HttpClient, private router:Router){}
-
-    autoLogin(){
-        const userData:{
-            id:string,
-            _token:string
-        } = JSON.parse(localStorage.getItem('userData'));
-        if(userData){
-            const user = new User(userData.id,userData._token);
-            this.user.next(user);
-        }else{
-            this.router.navigate(['/login']);
-        }
+  autoLogin() {
+    const userData: {
+      email: string;
+      _token: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (userData) {
+      const user = new User(userData.email, userData._token);
+      this.user.next(user);
+    } else {
+      this.router.navigate(['/login']);
     }
+  }
 
-    logout(){
-        this.user.next(null);
-        localStorage.removeItem('userData');
-    }
+  logout() {
+    this.user.next(null);
+    localStorage.removeItem('userData');
+  }
 
-    login(datos:any){
-        return this.http
-        .post<AuthResponseData>('http://127.0.0.1:3080/login',{
-            "id":datos.id,
-            "contrasena":datos.contrasena
+  login(datos: any) {
+    return (
+      this.http
+        .post<AuthResponseData>('http://127.0.0.1:8080/login', {
+          id: datos.id,
+          email: datos.email,
+          contrasena: datos.contrasena,
         })
         //Agregar Catch Error Despues
-        .pipe(tap(responseData=>{
-            
-            const user = new User(responseData.id,responseData.accessToken);
+        .pipe(
+          tap((responseData) => {
+            const user = new User(responseData.email, responseData.accessToken);
 
-            localStorage.setItem('userData',JSON.stringify(user));
-            
+            localStorage.setItem('userData', JSON.stringify(user));
             this.user.next(user);
-        }));
-    }
+          })
+        )
+    );
+  }
 
-    signUp(datos:any){
-        return this.http.post('http://127.0.0.1:8080/personas',
-        {
-          "nombre":datos.nombre,
-          "apellido":datos.apellido,
-          "contrasena":datos.contrasena,
-          "email":datos.email,
-          "telefono":datos.telefono,
-          "fecha_nacimiento":datos.fecha_nacimiento
-        });
-    }
-
-
-
+  signUp(datos: any) {
+    return this.http.post('http://127.0.0.1:8080/personas', {
+      nombre: datos.nombre,
+      apellido: datos.apellido,
+      hash_contrasena: datos.hash_contrasena,
+      email: datos.email,
+      fecha_nacimiento: datos.fecha_nacimiento,
+    });
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,55 +7,58 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
   error?: string;
 
   light_theme = false;
 
   isSignedUp = true;
   wrongPass = false;
-  
 
-  constructor(private authService:AuthService, private router:Router){
-
-  }
-  onSwitch(){
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private auth: AuthService
+  ) {}
+  onSwitch() {
     this.isSignedUp = !this.isSignedUp;
   }
-  
-  toggleTheme(){
-    this.light_theme = !this.light_theme
+
+  toggleTheme() {
+    this.light_theme = !this.light_theme;
   }
 
-  onSubmit(form: NgForm){
-    if(this.isSignedUp){
-      this.authService.login({
-        id:form.value.id_persona,
-        contrasena:form.value.contrasena
-      }).subscribe(
-        responseData=>{
-          this.router.navigate(['/'])
-          this.error = '';
-        },
-        resError=>{
-          this.error = resError.error
-        }
-        )
-    }else{
-      console.log(form)
-      this.authService.signUp({
-        nombre:form.value.nombre,
-        apellido:form.value.apellido,
-        contrasena:form.value.contrasena,
-        email:form.value.email,
-        telefono:form.value.telefono,
-        fecha_nacimiento:form.value.fecha_nacimiento
-      }).subscribe(responseData=>{
-        console.log(responseData)
-      })
+  onSubmit(form: NgForm) {
+    if (this.isSignedUp) {
+      this.authService
+        .login({
+          email: form.value.email,
+          contrasena: form.value.contrasena,
+        })
+        .subscribe(
+          (responseData) => {
+            this.router.navigate(['/']);
+            this.error = '';
+          },
+          (resError) => {
+            this.error = resError.error;
+          }
+        );
+    } else {
+      console.log(form);
+      this.authService
+        .signUp({
+          nombre: form.value.nombre,
+          apellido: form.value.apellido,
+          hash_contrasena: form.value.contrasena,
+          email: form.value.email,
+          fecha_nacimiento: form.value.fecha_nacimiento,
+        })
+        .subscribe((responseData) => {
+          console.log(responseData);
+        });
     }
   }
 }
